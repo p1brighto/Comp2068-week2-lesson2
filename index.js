222ad78abcbfe324433610d3b1076787e61054c9
p1brighto <p1brighto@gmail.com>
2016-01-21 15:17:26 -0500

    test1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import http = require('http');
 var hostname:string = '127.0.0.1';
 var port:number =process.env.PORT|| 8080;
 
-http.createServer(function(req:http.serverRequest))
+http.crea teServer(function(req:http.serverRequest))
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
