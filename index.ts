85988beb3ce00fd2e8eb978bf0ec64f123108bf2
p1brighto <p1brighto@gmail.com>
2016-02-01 02:27:32 -0500

    update the port local

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import http = require('http');
 
-var hostname:string = '127.0.0.1';
-var port:number = 8080;
+var port:number = process.env.PORT||8080;
 
 http.createServer(function(req:http.ServerRequest,res:http.ServerResponse){
     res.writeHead(200,{'Content-Type': 'text/plain'});
     res.end("Hello World\n");
-}).listen(port, hostname, function(){
+}).listen(port, function(){
     console.log("Server running at http://"+hostname +":"+port);
 });
