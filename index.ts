47a14f74c96d05df6054c7b5b756c516d9b10348
p1brighto <p1brighto@gmail.com>
2016-02-01 02:28:48 -0500

    removed the console

--- index.ts
+++ index.ts
@@ -5,6 +5,4 @@ var port:number = process.env.PORT||8080;
 http.createServer(function(req:http.ServerRequest,res:http.ServerResponse){
     res.writeHead(200,{'Content-Type': 'text/plain'});
     res.end("Hello World\n");
-}).listen(port, function(){
-    console.log("Server running at http://"+hostname +":"+port);
-});
+}).listen(port);
