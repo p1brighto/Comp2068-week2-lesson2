eab32cab727facd181d898a684b303bc6a85cc52
p1brighto <p1brighto@gmail.com>
2016-02-01 02:37:06 -0500

    changed to server from index

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end("Hello World\n");
 }).listen(port);
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=server.js.map
